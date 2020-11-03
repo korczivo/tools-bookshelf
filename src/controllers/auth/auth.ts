1dd12a2c2148e680757e998286cb552295aa9067
Łukasz Korcz <lukaszwp55@gmail.com>
2020-11-03 20:39:31 +0100

    setup prisma

--- src/controllers/auth/auth.ts
+++ src/controllers/auth/auth.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-import {
-    createUserService,
-    updateUserService
-} from "../../services/auth";
+import { PrismaClient } from '@prisma/client';
+import { createUserService, updateUserService } from '../../services/auth';
 
 const authController = express.Router();
+const prisma = new PrismaClient();
 
 authController.post('/auth/register', async (req, res) => {
-    await createUserService(req.body)
-    await updateUserService(req.body)
+  const allUsers = await prisma.users.findMany();
+  console.log(allUsers);
 });
 
 export { authController };
